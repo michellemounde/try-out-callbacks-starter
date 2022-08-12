13713cc62295d3534d9a95adf711fee42dba6316
Michelle Mounde <mitchmounde@gmail.com>
2022-08-12 21:03:47 +0300

    feat: complete callbacks problem sets

---
+++ problems/16-suffix-cipher.js
@@ -0,0 +1,63 @@
+/*******************************************************************************
+Write a function `suffixCipher` that accepts a sentence and object as arguments.
+The object contains suffixes as keys and callbacks as values. The `suffixCipher`
+function should return a new sentence where words of the original sentence are
+modified according to the callback that corresponds with the suffix that the word
+ends with. If the word does not end in any of the suffix keys, then it should not
+be modified. You can assume that only one suffix of the object will match a word.
+
+Examples:
+
+
+*******************************************************************************/
+
+let suffixCipher = function(sentence, obj) {
+    const words = sentence.split(" ");
+    const suffixed = [];
+
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        let ciphered;
+
+        for (let key in obj) {
+            let cb = obj[key];
+
+            if (word.endsWith(key)) {
+                let newWord = cb(word);
+                suffixed.push(newWord);
+                ciphered = true;
+            }
+        }
+
+        if (!ciphered) {
+            suffixed.push(word);
+        }
+    }
+
+    return suffixed.join(" ");
+};
+
+let cipher1 = {
+    ly: function(word) {
+        return word.slice(0, -1) + 'ee';
+    },
+    ize: function(word) {
+        return word + 'r';
+    }
+};
+console.log(suffixCipher('quietly and gently visualize', cipher1));
+// quietlee and gentlee visualizer
+
+let cipher2 = {
+    tal: function(word) {
+        return word.toUpperCase();
+    },
+    s: function(word) {
+        return word + 'th';
+    }
+};
+console.log(suffixCipher('incremental progress is very instrumental', cipher2));
+// INCREMENTAL progressth isth very INSTRUMENTAL
+
+/*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
+module.exports = suffixCipher;
